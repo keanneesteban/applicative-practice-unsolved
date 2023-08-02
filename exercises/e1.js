ea6deae6a7fec8997ebd3d6fb90bae194e41d9a9
keanneesteban <keanneesteban7@gmail.com>
2023-08-02 12:17:34 -0500

    completed ex 1

---
+++ exercises/e1.js
@@ -0,0 +1,17 @@
+import { data } from "../data/data";
+
+// SPACE DATA EXERCISE 1
+// Return an array of all Planets' names
+// Return example: ['name1', 'name2', ... , 'nameN']
+
+export function getPlanetNames(data) {
+  const allPlanetNames = data.planets.map((planet) => planet.name);
+  return allPlanetNames;
+}
+
+
+
+// === TEST YOURSELF ===
+// Once you're finished run the test with "npm run test-1"
+// If the test has all tests passed, switch to the next exercise file
+// If any of the tests fails, refactor the code and run the test command after you've fixed the function
