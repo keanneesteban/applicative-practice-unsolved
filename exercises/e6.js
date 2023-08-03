92704af120a153761ebb4560fe9f5b6299f34977
keanneesteban <keanneesteban7@gmail.com>
2023-08-03 13:41:17 -0500

    finished ex 6

--- exercises/e6.js
+++ exercises/e6.js
@@ -5,8 +5,12 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getAsteroidsDiscoveredAfterYear(data, year) {
-  // Your code goes here...
+  return data.asteroids
+    .filter((asteroid) => asteroid.discoveryYear > year)
+    .map((asteroid) => asteroid.name);
 }
+
+
 
 
 
