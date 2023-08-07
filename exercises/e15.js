eb65f93e1adf285e00fac8d6a2b8913b44ca879b
keanneesteban <keanneesteban7@gmail.com>
2023-08-07 12:39:34 -0500

    finished ex 15

--- exercises/e15.js
+++ exercises/e15.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithNoMoons(data) {
-  // Your code goes here...
+  return data.planets.filter((planet) => planet.moonsCount === 0 || planet.moonsCount === undefined)
+  .map((planet) => planet.name);
 }
 
 
